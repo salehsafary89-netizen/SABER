8992b89bb763727b78b9d193d80c7ca4a29c2c26
salehsafary89-netizen <salehsafary89@gmail.com>
2026-09-23 05:31:39 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'manch-rel-v3';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() =>
+      caches.match(req).then(cached => cached || caches.match('./index.html'))
+    )
+  );
+});
